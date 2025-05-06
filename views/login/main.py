35f9b4c114e56dabc7da8609d7a61e11327de9f0
Bruthalles <thallesgmkr@gmail.com>
2025-05-06 18:19:29 -0300

    iniciando tela login

--- views/login/main.py
+++ views/login/main.py
@@ -1,1 +1,12 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+janela = tk.Tk()
+janela.title("Emissão de Carteirinhas - Login")
+janela.resizable(False,False)
+ttk.Label(janela,text="comp label").grid(column=0,row=0)
+
+
+
+
+
+janela.mainloop()
