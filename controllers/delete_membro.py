adeceaa6e8f35b6bb5ddbf12f4bbea7523f30cf5
Bruthalles <thallesgmkr@gmail.com>
2025-05-06 16:44:40 -0300

    movendo crud

---
+++ controllers/delete_membro.py
@@ -0,0 +1,15 @@
+"""
+Autor: [Hugo Leonardo Fonseca de Pinho]
+Descrição: [Remove membro permanentemente. 
+Use com sabedoria não tem Ctrl+Z que te salve.]
+"""
+
+from db import conectar_banco
+
+def deletar_membro(id_membro):
+ conn = conectar_banco()
+ cursor = conn.cursor()
+ cursor.execute('DELETE FROM membros WHERE id = ?', (id_membro,))
+ conn.commit()
+ conn.close()
+ return cursor.rowcount
