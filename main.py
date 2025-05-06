e5551aab2c1264a59bc276aed1a354aec2b683b1
Bruthalles <thallesgmkr@gmail.com>
2025-05-06 17:41:27 -0300

    testando inputs dinamicos

--- main.py
+++ main.py
@@ -4,26 +4,43 @@ Descrição: [Arquivo principal que roda o sistema.]
 """
 
 from models.db import *
+from models import Membro
 from controllers.create_membro import criar_membro
 from controllers.read_membro import obter_membros, obter_membro_por_id
 from controllers.update_membro import atualizar_membro
 from controllers.delete_membro import deletar_membro
 
 if __name__ == "__main__":
- criar_tabela()
+    membro = Membro
+
+    membro.nome = input("digite nome do membro: ")
+    membro.data_nascimento = input("digite data de nascimento: ")
+    membro.email = input("digite email: ")
+    membro.cpf = input("digite cpf: ")
+    
+    print(membro)
+
+
+
+
+
+
+
+    # conectar_banco()
+    # criar_tabela()
     
     # Exemplo de uso
-novo_membro = {
-        'nome': 'Jair Luiz Messias Inácio Lulabolso da Silva',
-        'idade': 78,
-        'endereco': 'Av. do Caô , 171',
-        'email': 'Politicameuszovos@gmail.com'
-    }
+# novo_membro = {
+#         'nome': 'Jair Luiz Messias Inácio Lulabolso da Silva',
+#         'idade': 78,
+#         'endereco': 'Av. do Caô , 171',
+#         'email': 'Politicameuszovos@gmail.com'
+#     }
     
-id_novo = criar_membro(novo_membro)
-print(f"Membro criado com ID: {id_novo}")
+# id_novo = criar_membro(novo_membro)
+# print(f"Membro criado com ID: {id_novo}")
     
-print("\nLista de membros:")
-for membro in obter_membros():
-    print(membro)
+# print("\nLista de membros:")
+# for membro in obter_membros():
+#     print(membro)
 
