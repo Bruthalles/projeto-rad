051a2f4414ed7e1c9c3a73ca444ab4422b393611
Bruthalles <thallesgmkr@gmail.com>
2025-05-08 03:16:19 -0300

    classe BancoDeDados finalizada

--- models/Membro.py
+++ models/Membro.py
@@ -1,23 +1,8 @@
-# nome : str
-#     - data_nascimento : date
-#     - email : str 
-#     - cpf : str
-#     - data_cadastro : date 
-#     + maior_de_idade(): bool 
-#     + validar_cpf(): bool 
-
-import datetime 
 class Membro:
 
-    nome: str
-    data_nascimento: str 
-    email: str
-    cpf: str
-
-    def __init__(self,nome,data_nascimento,email,cpf):
+    def __init__(self,nome,data_nascimento,cpf,email):
         self.nome = nome
         self.data_nascimento = data_nascimento 
         self.email = email
         self.cpf = cpf
-
     
--- models/db.py
+++ models/db.py
@@ -2,30 +2,39 @@
 Autor: [Hugo Leonardo Fonseca de Pinho]
 Descrição: [Base do sistema, cria o banco e a tabela inicial.
 Se tá funcionando, não mexe. (Aprendi num tutorial de 2015 no youtube)]
+Copiloto: Thalles Brumatti :D editando algumas coisas...
 """
 
-import sqlite3
+import sqlite3,os
+class BancoDeDados:
 
-def conectar_banco():
- return sqlite3.connect('clube.db')
+    def __init__(self):
+        self.conn = None
+    
+    
+    def conectar_banco(self):
+        #criando db no mesmo diretorio
+        path = os.path.join(os.path.dirname(__file__),'clube.db')
+        self.conn = sqlite3.connect(path)
+        return self.conn
+    
+    
+    def criar_tabela(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS membros (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                data_nascimento DATE,
+                cpf TEXT NOT NULL,
+                email TEXT UNIQUE,
+                data_cadastro DATE DEFAULT (date('now')),
+                status TEXT CHECK(status IN ('Ativo', 'Inativo')) DEFAULT 'Ativo'
+                )
+            ''')
+        self.conn.commit()
 
-def criar_tabela():
- conn = conectar_banco()
- cursor = conn.cursor()
- cursor.execute('''
-    CREATE TABLE IF NOT EXISTS membros (
-        
-        nome TEXT NOT NULL,
-        data_nascimento DATE,
-        cpf TEXT PRIMARY KEY NOT NULL,
-        email TEXT UNIQUE,
-        data_cadastro DATE DEFAULT (date('now')),
-        status TEXT CHECK(status IN ('Ativo', 'Inativo')) DEFAULT 'Ativo'
-        )
-    ''')
- conn.commit()
-
- def fechar_conexao():
-    if conn:
-        conn.close()
-        conn = None
+    
+    def fechar_conexao(self):
+        self.conn.close()
+        self.conn = None
