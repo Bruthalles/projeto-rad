a81c64631cbff7020305495b7f164a503e0e9898
Bruthalles <thallesgmkr@gmail.com>
2025-05-08 15:51:25 -0300

    limpando a main

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-from models.Usuario import Usuario
-
 if __name__ == '__main__':
-    admin = Usuario
-    user = input('digite o nome de usuário: ')
-    senha = input("digete a senha: ")
-    
-    admin.validar_usuario(admin,user,senha)
+    pass
