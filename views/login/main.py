e81223ef8d33b220c551088902a0f196ee322357
Bruthalles <thallesgmkr@gmail.com>
2025-05-09 18:54:20 -0300

    atualizando login

--- views/login/main.py
+++ views/login/main.py
@@ -1,12 +1,15 @@
-import tkinter as tk
-from tkinter import ttk
-janela = tk.Tk()
-janela.title("Emissão de Carteirinhas - Login")
-janela.resizable(False,False)
-ttk.Label(janela,text="comp label").grid(column=0,row=0)
+from tkinter import * 
+
+pg_login = Tk()
+pg_login.title("Emissão de Carteirinhas - Login")
+pg_login.resizable(False,False)
+txt_login = Label(pg_login,text="Login")
+txt_login.grid(column=0,row=0)
+
+txt_usr = Label(pg_login,text='Usuário')
+txt_usr.grid(column=0,row=1)
 
 
 
 
-
-janela.mainloop()
+pg_login.mainloop()
