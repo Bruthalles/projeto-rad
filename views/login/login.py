f8c3c03754036921d839ecd29db7800e6adc5b0e
Bruthalles <thallesgmkr@gmail.com>
2025-05-12 16:47:32 -0300

    update login.py

--- views/login/login.py
+++ views/login/login.py
@@ -10,8 +10,10 @@ load_dotenv()
 user_name = str(os.getenv("NOME_USER"))
 pass_user = str(os.getenv("PASS_USER"))
 
+# ""encapsulando"" a tela login
 def Pg_Login():
-        
+    
+    #definindo a estrutura e formas
     pg_login = Tk()
     pg_login.geometry('900x500')
     frame = Frame(pg_login)
@@ -20,6 +22,7 @@ def Pg_Login():
 
     pg_login.title("Emissão de Carteirinhas - Login")
 
+    #Componentes
     txt_login = Label(pg_login,text="Login",font=("Arial",20))
     txt_login.pack(pady=(20,40))
 
@@ -38,12 +41,13 @@ def Pg_Login():
     aviso = Label(pg_login,text="",font=("Arial",12,"bold"))
     aviso.pack(pady=(5,5))
 
+    #pegando valor do elemento Entry
     def get_user():
-
         nm_user = str(field_usr.get())
         ps_user = str(field_senha.get())
         return nm_user,ps_user
 
+    #validação simples e troca de tela
     def validar_usuario(in_user,in_pass):
         
         if(in_user == user_name and in_pass == pass_user):
@@ -52,11 +56,13 @@ def Pg_Login():
             Pg_Home()
         else: 
             aviso.config(text="Usuario incorreto",fg="red")
-        
+    
+    #simplificando a função para ser chamada
     def login():
         nm_user , ps_user = get_user()
         validar_usuario(nm_user,ps_user)
 
+    #parametro command deve receber uma função sem parenteses para não executar imediatamente
     entrar = Button(pg_login,text="Entrar",height=2,width=17,bg="blue",fg="white",font=("Arial",11,"bold"),command=login)
     entrar.pack(pady=(20,50))
 
