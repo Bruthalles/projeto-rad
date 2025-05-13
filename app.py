49e99ff8f7d6bd5341c33d96c15f8bf6f9bbdf27
Bruthalles <thallesgmkr@gmail.com>
2025-05-13 19:31:29 -0300

    update venv

--- app.py
+++
@@ -1,4 +0,0 @@
-from views.login.login import Pg_Login
-
-if __name__ == "__main__":
-    Pg_Login()
--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-Autor: [Hugo Leonardo Fonseca de Pinho]
-Descrição: [Arquivo principal que roda o sistema.]
-"""
-from views.login.login import Pg_Login
-
-if __name__ == "__main__":
-    Pg_Login()
